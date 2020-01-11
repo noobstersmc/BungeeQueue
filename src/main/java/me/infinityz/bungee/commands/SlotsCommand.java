df06844b4c11a7f576736ef59259faf93a0d085b
InfinityZ25 <henixceo@gmail.com>
2020-01-11 17:05:56 -0500

    Some cleaning up

--- src/main/java/me/infinityz/bungee/commands/SlotsCommand.java
+++ src/main/java/me/infinityz/bungee/commands/SlotsCommand.java
@@ -2,6 +2,7 @@ package me.infinityz.bungee.commands;
 
 import me.infinityz.bungee.QueuePlugin;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
 
 /**
@@ -16,15 +17,15 @@ public class SlotsCommand extends Command {
     @Override
     public void execute(CommandSender sender, String[] args) {
         if (args.length < 1) {
-            sender.sendMessage("Correct command usage: /globalslots <int:current>");
+            sender.sendMessage(new TextComponent("Correct command usage: /globalslots <int:current>"));
             return;
         }
         if (args[0].equalsIgnoreCase("current")) {
-            sender.sendMessage("Current max capacity: " + QueuePlugin.global_slots);
+            sender.sendMessage(new TextComponent("Current max capacity: " + QueuePlugin.global_slots));
             return;
         }
         int integer = Integer.parseInt(args[0]);
-        sender.sendMessage("New max capacity is: " + integer);
+        sender.sendMessage(new TextComponent("New max capacity is: " + integer));
         QueuePlugin.global_slots = integer;
 
     }
