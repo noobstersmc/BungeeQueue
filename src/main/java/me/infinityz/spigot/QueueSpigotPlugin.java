41d4df098ff2b0449fb8c87e8946d69860b3b888
InfinityZ25 <henixceo@gmail.com>
2020-01-11 17:14:57 -0500

    Bracnhing out from bungee into spigot development

---
+++ src/main/java/me/infinityz/spigot/QueueSpigotPlugin.java
@@ -0,0 +1,16 @@
+package me.infinityz.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * QueueSpigotPlugin
+ */
+public class QueueSpigotPlugin extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+
+    
+}
