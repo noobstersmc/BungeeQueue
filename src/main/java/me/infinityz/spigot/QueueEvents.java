943a1cadd89fd148a6dec20329188a9e1af94d89
InfinityZ25 <henixceo@gmail.com>
2020-01-14 22:02:42 -0500

    changes

--- src/main/java/me/infinityz/spigot/QueueEvents.java
+++ src/main/java/me/infinityz/spigot/QueueEvents.java
@@ -1,8 +1,10 @@
 package me.infinityz.spigot;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 /**
  * QueueEvents
@@ -11,10 +13,20 @@ public class QueueEvents implements Listener {
     
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        if (e.getPlayer().hasPermission("queue"))
-            return;
-        
+        e.getPlayer().setGameMode(GameMode.SPECTATOR);
+        e.getPlayer().sendTitle("Welcome to the Limbo!", "Have fun doing nothing");
     }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e){
+        if(e.getTo().getBlockX() != e.getFrom().getBlockX() ||  e.getTo().getBlockZ() != e.getFrom().getBlockZ()){
+            e.setCancelled(true);
+        }
+        if(e.getTo().getBlockY() < 1){
+            e.getPlayer().teleport(e.getFrom().add(0.0, 40, 0.0));
+        }
+    }
+    
 
     
 }
