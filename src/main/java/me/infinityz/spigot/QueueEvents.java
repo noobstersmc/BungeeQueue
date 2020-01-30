eb8b77648db4e2b072defddd447a6ec34eee71a7
AleIV <jorgefcorralesmayorga@iMac-de-Jorge.local>
2020-01-30 11:30:14 -0600

    Change on respawn mechanism

--- src/main/java/me/infinityz/spigot/QueueEvents.java
+++ src/main/java/me/infinityz/spigot/QueueEvents.java
@@ -2,13 +2,14 @@ package me.infinityz.spigot;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPistonExtendEvent;
@@ -16,6 +17,7 @@ import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 /**
  * QueueEvents
@@ -25,46 +27,52 @@ public class QueueEvents implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.setJoinMessage("");
-        if(e.getPlayer().hasPlayedBefore())return;
-        
-        Location loc = new Location(e.getPlayer().getWorld(), 0, 0, 0);
-        loc.setX(loc.getX() + Math.random() * 1000 * 2.0 - 1000);
-        loc.setZ(loc.getZ() + Math.random() * 1000 * 2.0 - 1000);
-        loc = loc.getWorld().getHighestBlockAt(loc).getLocation().add(0.0, 2.0, 0.0);
-        e.getPlayer().teleport(loc);
+        if (e.getPlayer().hasPlayedBefore())
+            return;
+
+        e.getPlayer().teleport(getRandomLocation(1000, Bukkit.getWorlds().get(0)));
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
         e.setQuitMessage("");
     }
-    // Cancel craft slime/honey blocks
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
         if (e.getEntity().hasPermission("queue.override"))
             return;
-            Bukkit.getScheduler().runTaskLater(AnarchySpigotPlugin.instance, () -> {
+        Bukkit.getScheduler().runTaskLater(AnarchySpigotPlugin.instance, () -> {
+            e.getEntity().kickPlayer(
+                    ChatColor.translateAlternateColorCodes('&', "&fYou died, adquire &6rank&f to play freely!"));
+
+        }, 10L);
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e){
+        final Player player = e.getPlayer();
+        e.setRespawnLocation(getSpawnLocation(player));
+    }
     
-                e.getEntity().spigot().respawn();
-                Location loc = new Location(e.getEntity().getWorld(), 0, 0, 0);
-                loc.setX(loc.getX() + Math.random() * 1000 * 2.0 - 1000);
-                loc.setZ(loc.getZ() + Math.random() * 1000 * 2.0 - 1000);
-                loc = loc.getWorld().getHighestBlockAt(loc).getLocation().add(0.0, 2.0, 0.0);
-                e.getEntity().teleport(loc);
-            }, 1L);
-            Bukkit.getScheduler().runTaskLater(AnarchySpigotPlugin.instance, () -> {
-    
-                e.getEntity().kickPlayer(
-                        ChatColor.translateAlternateColorCodes('&', "&fYou died, adquired &6rank&f to play freely!"));
-    
-            }, 10L);
+    //Function that allows for the player to be spawn either randomly or at their bed spawn.
+    Location getSpawnLocation(Player player){
+        return player.getBedSpawnLocation() == null ? getRandomLocation(1000, Bukkit.getWorlds().get(0)) : player.getBedSpawnLocation();
+    }
+
+    Location getRandomLocation(int radius, World world){
+        Location loc = new Location(world, 0, 0, 0);
+        loc.setX(loc.getX() + (Math.random() * radius * 2.0) - radius);
+        loc.setZ(loc.getZ() + (Math.random() * radius * 2.0) - radius);
+        loc.setY(loc.getWorld().getHighestBlockYAt(loc.getBlockX(), loc.getBlockZ()));
+        loc = loc.add(0.0, 2.0, 0.0);
+        return loc;      
     }
 
     @EventHandler
     public void onCraft(BlockPistonExtendEvent e) {
         Block b = e.getBlock();
-        List list = getNearbyBlocks(b.getLocation(), 3);
+        List<Block> list = getNearbyBlocks(b.getLocation(), 3);
         if (list.size() > 0)
             e.setCancelled(true);
 
@@ -73,7 +81,7 @@ public class QueueEvents implements Listener {
     @EventHandler
     public void onCraft(BlockPistonRetractEvent e) {
         Block b = e.getBlock();
-        List list = getNearbyBlocks(b.getLocation(), 3);
+        List<Block> list = getNearbyBlocks(b.getLocation(), 3);
         if (list.size() > 0)
             e.setCancelled(true);
 
@@ -85,7 +93,7 @@ public class QueueEvents implements Listener {
             for (int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
                 for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
                     Block b = location.getWorld().getBlockAt(x, y, z);
-                    if (b.getType() == Material.HONEY_BLOCK || b.getType() == Material.SLIME_BLOCK)
+                    if (b.getType() == Material.SLIME_BLOCK)
                         blocks.add(b);
                 }
             }
