a87a0d30b4cd63ab69df57095c43251c71668404
AleIV <jorgefcorralesmayorga@iMac-de-Jorge.local>
2020-01-30 10:53:37 -0600

    Small changes

--- src/main/java/me/infinityz/bungee/QueuePlugin.java
+++ src/main/java/me/infinityz/bungee/QueuePlugin.java
@@ -7,7 +7,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
